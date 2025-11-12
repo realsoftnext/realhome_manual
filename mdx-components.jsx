42e95b2ed0e478f689f2cab26165a9e0fe5bd7e6
runai <85218477+renextkorea@users.noreply.github.com>
2025-11-12 13:11:33 +0900

    Fix Vercel configuration and MDX components

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -1,5 +1,15 @@
 export function useMDXComponents(components) {
   return {
+    h1: (props) => <h1 {...props} />,
+    h2: (props) => <h2 {...props} />,
+    h3: (props) => <h3 {...props} />,
+    p: (props) => <p {...props} />,
+    a: (props) => <a {...props} />,
+    ul: (props) => <ul {...props} />,
+    ol: (props) => <ol {...props} />,
+    li: (props) => <li {...props} />,
+    code: (props) => <code {...props} />,
+    pre: (props) => <pre {...props} />,
     ...components,
   }
 }
