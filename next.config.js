0eec99689125aefcbb15edf18d8ed09ef36db6c4
runai <85218477+renextkorea@users.noreply.github.com>
2025-11-12 13:18:05 +0900

    Fix module system: convert to CommonJS and add Vercel config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import createMDX from '@next/mdx'
+const createMDX = require('@next/mdx')
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
@@ -8,11 +8,9 @@ const withMDX = createMDX({
   },
 })
 
-export default withMDX({
+module.exports = withMDX({
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  // Vercel에서는 SSR 모드로 배포 (한글 인코딩 문제 해결)
-  // output: 'export', // 제거 - Vercel이 서버사이드 렌더링으로 처리
   trailingSlash: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
